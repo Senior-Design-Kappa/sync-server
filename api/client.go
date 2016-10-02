5e0a9a5577ccad31fb5edefc01a0319091270131
davlia <danedaworld@gmail.com>
2016-10-01 21:59:42 -0400

    v1 skeleton

---
+++ api/client.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Client is an middleman between the websocket connection and the hub.
+type Client struct {
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
